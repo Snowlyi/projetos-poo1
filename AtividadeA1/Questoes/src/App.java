0a9023b1b1b0838b8c4d5436a69f96e0b76652cb
Snowlyi <snowlyiszds@gmail.com>
2025-03-22 16:26:01 -0400

    Commit questão 02

---
+++ AtividadeA1/Questoes/src/App.java
@@ -0,0 +1,22 @@
+import dominio.Questao02.Questao02;
+import dominio.Questao03.Questao03;
+import dominio.Questao04.Questao04;
+import dominio.Questao05.Questao05;
+import dominio.BaseQuestoes;
+import dominio.Questao01.Questao01;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        BaseQuestoes ex01 = new Questao01();
+        ex01.Executar();
+        BaseQuestoes ex02 = new Questao02();
+        ex02.Executar();
+        //BaseQuestoes ex03 = new Questao03();
+        //ex03.Executar();
+        //BaseQuestoes ex04 = new Questao04();
+        //ex04.Executar();
+        //BaseQuestoes ex05 = new Questao05();
+        //ex05.Executar();   
+    
+    }
+}
---
+++ AtividadeA1/Questoes/src/dominio/Questao02/Questao02.java
@@ -0,0 +1,80 @@
+package dominio.Questao02;
+
+import dominio.BaseQuestoes;
+
+public class Questao02 extends BaseQuestoes{
+    private Long base1, altura1, area1, base2, altura2, area2;
+
+    public Long getBase1() {
+        return base1;
+    }
+
+    public void setBase1(Long base1) {
+        this.base1 = base1;
+    }
+
+    public Long getAltura1() {
+        return altura1;
+    }
+
+    public void setAltura1(Long altura1) {
+        this.altura1 = altura1;
+    }
+
+    public Long getArea1() {
+        return area1;
+    }
+
+    public void setArea1(Long area1) {
+        this.area1 = area1;
+    }
+    public Long getBase2() {
+        return base2;
+    }
+
+    public void setBaseB(Long base2) {
+        this.base2 = base2;
+    }
+
+    public Long getAltura2() {
+        return altura2;
+    }
+
+    public void setAltura2(Long altura2) {
+        this.altura2 = altura2;
+    }
+    public Long getArea2() {
+        return area2;
+    }
+
+    public void setArea2(Long area2) {
+        this.area2 = area2;
+    }
+    
+    public void Executar() {
+        System.out.println("----- Questão 2 - Comparar a área de dois retângulos -----");
+        System.out.println("Insira a base e a altura do primeiro retângulo, em centímetros, respectivamente: ");
+        base1 = scanear.nextLong();
+        altura1 = scanear.nextLong();
+        System.out.println("Insira a base e a altura do segundo retângulo, em centímetros, respectivamente: ");
+        base2 = scanear.nextLong();
+        altura2 = scanear.nextLong();
+        System.out.println("-----------------------------------------------------------");
+        area1 = calcularArea(base1, altura1);
+        area2 = calcularArea(base2, altura2);
+
+            if (area1 == area2) {
+                System.out.println("As duas áreas são iguais");
+            } 
+            else if (area1 > area2) {
+                System.out.println("A área do primeiro retângulo é maior, com " + (area1 - area2) + "cm de diferença"); 
+            }
+            else {System.out.println("A área do segundo retãngulo é maior, com " + (area2 - area1) + "cm de diferença");
+            }
+        System.out.println("-----------------------------------------------------------");
+    }
+
+    public Long calcularArea(Long x, Long y) {
+        return x * y;
+    }
+}
